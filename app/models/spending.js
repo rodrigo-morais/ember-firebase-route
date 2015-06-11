70b1a0a03923d190a011327c34c7de64d8c9296c
Rodrigo Morais <morais.rm@gmail.com>
2015-06-11 18:30:51 -0300

    Adding a computed property to take the name of month.

--- app/models/spending.js
+++ app/models/spending.js
@@ -3,5 +3,9 @@ import DS from "ember-data";
 export default DS.Model.extend({
   date: DS.attr('string'),
   item: DS.attr('string'),
-  value: DS.attr('string')
+  value: DS.attr('string'),
+  month: function(){
+    var _month = this.get('date').split('/')[1];
+    return new Date(1,_month,1).toLocaleString('en',{month:"long"});
+  }.property('date')
 });
