31cbad76e722b3a1d3b017babd7c03740e115c61
Rodrigo Morais <morais.rm@gmail.com>
2015-06-12 11:59:27 -0300

    Test conditional in Handlebars template.

--- app/models/spending.js
+++ app/models/spending.js
@@ -7,5 +7,17 @@ export default DS.Model.extend({
   month: function(){
     var _month = this.get('date').split('/')[1];
     return new Date(1,_month,1).toLocaleString('en',{month:"long"});
+  }.property('date'),
+  daysBehind: function(){
+    var now = Date.now();
+    var date = new Date(this.get('date').split('/')[2], this.get('date').split('/')[1] - 1,this.get('date').split('/')[0]);
+    
+    return parseInt((now - date)/(1000*60*60*24));
+  }.property('date'),
+  actualMonth: function(){
+    var now = new Date();
+    var date = new Date(this.get('date').split('/')[2], this.get('date').split('/')[1] - 1,this.get('date').split('/')[0]);
+
+    return now.getMonth() === date.getMonth();
   }.property('date')
 });
